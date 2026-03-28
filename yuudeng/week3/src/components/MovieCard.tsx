017575734268f16702b9ce1b235b446b34ca00b0
유정 <choiyu0wo0d@tukorea.ac.kr>
2026-03-28 18:52:47 +0900

    style: 가운데정렬

--- yuudeng/week3/src/components/MovieCard.tsx
+++ yuudeng/week3/src/components/MovieCard.tsx
@@ -16,7 +16,7 @@ export default function MovieCard({ movie }: MovieCardProps) {
       onMouseLeave={() => setIsHovered(false)}
     >
       <img
-        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={`${movie.title} 영화의 이미지`}
         className=""
       />
@@ -25,7 +25,7 @@ export default function MovieCard({ movie }: MovieCardProps) {
         <div
           className="absolute inset-0 bg-linear-to-t from-black/50 
         to-transparent backdrop-blur-md flex flex-col justify-center 
-        items-center text-white p-4"
+        items-center text-center text-white p-4"
         >
           <h2 className="text-lg font-bold leading-snug">{movie.title}</h2>
           <p className="test-sm text-gray-300 leading-relaxed mt-2 line-clamp-5">
