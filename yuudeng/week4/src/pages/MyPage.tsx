f492d27f95b8f4f547a8c2813b1ef3b8a5d759fa
유정 <choiyu0wo0d@tukorea.ac.kr>
2026-04-06 21:20:43 +0900

    feat: 미션3

---
+++ yuudeng/week4/src/pages/MyPage.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { getMyInfo } from "../apis/auth";
+import type { ResponseMyInfoDto } from "../types/auth";
+
+const MyPage = () => {
+  const [data, setData] = useState<ResponseMyInfoDto | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getMyInfo();
+      console.log(response);
+      setData(response);
+    };
+    getData();
+  }, []);
+
+  return <div className="text-white">{data?.data.name}님의 페이지입니다.</div>;
+};
+
+export default MyPage;
