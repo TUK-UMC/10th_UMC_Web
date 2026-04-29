ecb69eb7674a4f14d00e8ad99c73858b2bac38fe
Yun Yejin <yyjin0405@gmail.com>
2026-04-29 23:37:07 +0900

    feat: 5주차 미션

---
+++ chapter5/chapter5/src/layouts/ProtectedLatout.tsx
@@ -0,0 +1,19 @@
+import { Navigate, NavLink, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export const ProtectedLayout = () => {
+    const { accessToken } = useAuth();
+
+    if (!accessToken) {
+    return <Navigate to={"login"} replace />;
+    }
+    return (
+    <div className="h-dvh flex flex-col">
+        <NavLink to="/">홈</NavLink>
+        <main className="flex-1">
+        <Outlet />
+        </main>
+        <footer>푸터</footer>
+    </div>
+    );
+};
