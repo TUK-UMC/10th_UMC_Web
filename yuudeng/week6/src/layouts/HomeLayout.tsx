170bc882d2a090f422eb17db3aed073dfc5cd348
유정 <choiyu0wo0d@tukorea.ac.kr>
2026-05-04 01:12:35 +0900

    chore: 6주차 폴더 생성

---
+++ yuudeng/week6/src/layouts/HomeLayout.tsx
@@ -0,0 +1,41 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const HomeLayout = () => {
+  return (
+    <>
+      <div className="h-dvh flex flex-col bg-black">
+        <div className="flex justify-between items-center p-4 bg-[#141414]">
+          <NavLink to="/" className="text-xl font-bold text-[#E63996]">
+            돌려돌려 돌림판
+          </NavLink>
+          <div className="flex gap-4 text-md">
+            <NavLink
+              to="/login"
+              className="flex bg-black text-white py-2 px-4 rounded-md"
+            >
+              로그인
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className="flex bg-[#E63996] text-white py-2 px-4 rounded-md"
+            >
+              회원가입
+            </NavLink>
+            <NavLink
+              to="/my"
+              className="flex bg-[#E63996] text-white py-2 px-4 rounded-md"
+            >
+              마이페이지
+            </NavLink>
+          </div>
+        </div>
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <footer>푸터</footer>
+      </div>
+    </>
+  );
+};
+
+export default HomeLayout;
