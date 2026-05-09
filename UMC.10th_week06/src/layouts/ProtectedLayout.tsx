8ce5455407826ca9c4047440e4ab7155c19396c4
breezu-art <eunj8671@gmail.com>
2026-05-10 03:29:13 +0900

    zu.10th_week06

---
+++ UMC.10th_week06/src/layouts/ProtectedLayout.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import Navbar from "../components/Navbar";
+
+const ProtectedLayout = () => {
+  const { accessToken } = useAuth();
+
+  if (!accessToken) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return (
+    <div className="h-dvh flex flex-col">
+      <Navbar />
+      <main className="flex-1 mt-10>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default ProtectedLayout;
