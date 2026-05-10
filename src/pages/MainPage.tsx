3b1da03f0bb87ec42484759c39fa54c092b16e14
breezu-art <eunj8671@gmail.com>
2026-05-11 00:49:45 +0900

    Clean up and push

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,60 @@
+import { useIsFetching } from "@tanstack/react-query";
+import { PAGINATION_ORDER } from "../enums/common";
+import useGetInfiniteLpList from "../hooks/queries/useGetInfiniteLpList";
+import useGetLpList from "../hooks/queries/useGetLpList";
+import type { Lp, ResponseLpListDto } from "../types/lp";
+import { useState, useEffect } from "react";
+import LpCardSkeletonList from "../components/LpCard/LpCardSkeletonList";
+import LpCard from "../components/LpCard/LpCard";
+import {useInView} from "react-intersection-observer"
+
+const MainPage = () => {
+  const [search, setSearch] = useState("");
+//  const {data, isPending, isError} = useGetLpList({
+//    search,
+//    limit:50,
+//  });
+
+  const {data:lps, isFetching, hasNextPage, isPending, fetchNextPage, isError} = 
+    useGetInfiniteLpList(10, search, PAGINATION_ORDER.asc);
+
+  // ref -> 특정한 HTML 요소를 감시할 수 있다.
+  // inView -> 그 요소가 화면에 보이면 true
+  const {ref, inView} = useInView({
+    threshold:0,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  },[inView, isFetching, hasNextPage, fetchNextPage]);
+
+  if (isPending) {
+    return <div className={"mt-20"}>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div className={"mt-20"}>Error...</div>;
+  }
+
+  console.log(lps);
+
+  return (
+    <div className="container mx-auto px-4 py-6">
+      <input value={search} onChange={(e) => setSearch(e.target.value)}/>
+
+      <div className={"grid grid-cols-1 sm:grid-cols-2 md:grid-col-3 lg:grid-cols-4 gap-4"}>
+        {lps?.pages ?.map((page) => page.data.data)
+        ?.flat()
+        ?.map((lp:Lp) => <LpCard key={lp.id} lp={lp}/>)} 
+
+        {isFetching && <LpCardSkeletonList count={20}/>}
+      </div>
+      <div ref={ref} className="h-2"></div>
+    </div>
+  );
+};
+
+
+export default MainPage;
